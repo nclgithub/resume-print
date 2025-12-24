a78ba47010293583bd0eca3f83a408468c0faeab
nclgithub <ngchunliangy@gmail.com>
2025-12-24 16:05:02 +0800

    fix bug

--- app/main/print/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function PrintPage() {
-    return (
-        <div style={{ width: "100vw", height: "100vh" }}>
-            {/* <iframe src={null} width="100%" height="100%" /> */}
-        </div>
-    );
-}
