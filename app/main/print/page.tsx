697367989f63b16479f799a1a680ef9cc8c2c0f5
nclgithub <ngchunliangy@gmail.com>
2025-12-24 16:02:05 +0800

    fix bug

--- app/main/print/page.tsx
+++ app/main/print/page.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import { useRouter } from 'next/router';
-
 export default function PrintPage() {
-    const router = useRouter();
-    const { data } = router.query;
-    const src = Array.isArray(data) ? data[0] : (data || '');
-
     return (
         <div style={{ width: "100vw", height: "100vh" }}>
-            <iframe src={src} width="100%" height="100%" />
+            {/* <iframe src={null} width="100%" height="100%" /> */}
         </div>
     );
 }
